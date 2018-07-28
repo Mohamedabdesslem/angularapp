c7814747c4d6545f2729ff757d9a516236c2deee
abdesslem <abdesslem.boudabbous@estiam.com>
2018-07-28 20:12:22 +0200

    Revert "2eme push" aaaaaaaa This reverts commit 75a1ad5a40f90e3280a33879858bbb89353086a7.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ContactsComponent } from './contacts/contacts.component';
-
 
 @NgModule({
   declarations: [
@@ -13,7 +12,7 @@ import { ContactsComponent } from './contacts/contacts.component';
     ContactsComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
